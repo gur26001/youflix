531c0bcfa21e98641c2750b4c83e1422f376d93e
Gursimran Singh <gurisingh262001@gmail.com>
2022-09-06 11:09:37 +0530

    single video  page structure setup changed

--- src/components/Singlevideo.js
+++ src/components/Singlevideo.js
@@ -1,7 +1,25 @@
-export default function Singlevideo(){
-    return(
+import './Singlevideo.css';
+
+export default function Singlevideo() {
+    return (
         <div>
-            
+            <div className="leftSection">
+                <div className='videoSection'>
+
+                </div>
+                <div className='channelSection'>
+
+                </div>
+                <div className='commentSection'>
+
+                </div>
+            </div>
+            <div className="rightSection">
+                <h1 >FILTER</h1>
+                <div className='recommededVideos'>
+
+                </div>
+            </div>
         </div>
     );
 }
