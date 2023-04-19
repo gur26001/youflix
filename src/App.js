ecae0cd46b5a022aafdf262eb4f59ac9c4e98c7a
Gursimran Singh <gurisingh262001@gmail.com>
2023-04-19 21:13:36 +0530

    directory paths of components corrected

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 import './App.css';
-import Sidebar from './Components/Sidebar.js';
-import Header from './Components/Header.js';
-import RecommendedVideos from './Components/RecommendedVideos.js';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import Singlevideo from './Components/Singlevideo.js';
-
-
+import Sidebar from '../src/Components/Sidebar';
+import Header from '../src/Components/Header.js';
+import RecommendedVideos from '../src/Components/RecommendedVideos.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Singlevideo from '../src/Components/Singlevideo.js';
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
+	return (
+		<div className="app">
+			<Router>
+				{/* header */}
+				<Header />
+				<div className="app_p">
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<div className="app_page">
+									<Sidebar />
+									<RecommendedVideos />
+								</div>
+							}
+						/>
 
-        {/* header */}
-        <Header />
-        <div className='app_p'>
-          <Routes>
-            <Route path="/" element={     
-                    <div className='app_page' >
-                      <Sidebar/>
-                      <RecommendedVideos/>
-                    </div>    
-                    
-                
-            } />
-            
-            <Route path='/video/:id' element={<Singlevideo />} />
-
-        </Routes>
-  
-        </div>
-
-        </Router>
-
-    
-    </div>
-  );
+						<Route path="/video/:id" element={<Singlevideo />} />
+					</Routes>
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
